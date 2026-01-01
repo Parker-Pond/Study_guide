0c688093b0ac8f76e15b1a79eb737c93456e08c6
Parker-Pond <parker.a.pond@gmail.com>
2026-01-01 14:53:56 -0700

    added basic sorting algorithm information

---
+++ Basics/sorting_algorithms.js
@@ -0,0 +1,7 @@
+//$ .sort()
+// ! Sorts in unicode AKA number (converts letters to number)
+//? Modifies the original array
+
+//$ .toSorted()
+// ! Sorts in unicode AKA number (converts letters to number)
+//? creates a shallow copy of the original array
